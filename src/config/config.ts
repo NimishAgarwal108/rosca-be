2223acca1bf92d8879b31d3ef7d764891993ff1f
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-11-27 04:25:37 +0530

    added continue with google

--- src/config/config.ts
+++ src/config/config.ts
@@ -92,7 +92,7 @@ const config: Config = {
   googleOAuth: {
     clientId: process.env.GOOGLE_CLIENT_ID || '',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    redirectUri: process.env.GOOGLE_REDIRECT_URI || 'http://localhost:3000/api/auth/google/callback',
+    redirectUri: process.env.GOOGLE_REDIRECT_URI,
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email'
